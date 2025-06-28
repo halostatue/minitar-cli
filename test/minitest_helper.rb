5fe3439fa3c0e42d649a6131e210fc2703c5b07a
Austin Ziegler <austin@zieglers.ca>
2025-06-28 16:46:52 -0400

    chore: Windows 2019 runners are deprecated
    Signed-off-by: Austin Ziegler <austin@zieglers.ca>

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -10,3 +10,9 @@ require "minitest/focus"
 Dir.glob(File.join(File.dirname(__FILE__), "support/*.rb")).sort.each do |support|
   require support
 end
+
+if ENV["STRICT"]
+  $VERBOSE = true
+  Warning[:deprecated] = true
+  require "minitest/error_on_warning"
+end
